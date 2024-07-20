f567e231abc887df9157847b135ac03aecd138b7
AEBC08 <125038182+AEBC08@users.noreply.github.com>
2024-07-20 10:23:56 +0800

    Add files via upload

---
+++ PluginTemplate/PluginTemplate.py
@@ -0,0 +1,17 @@
+flora_api = {}  # 顾名思义,FloraBot的API,载入(若插件已设为禁用则不载入)后会赋值上
+
+
+def occupying_function(*values):  # 该函数仅用于占位,并没有任何意义
+    pass
+
+
+send_msg = occupying_function
+
+
+def init():  # 插件初始化函数,在载入(若插件已设为禁用则不载入)或启用插件时会调用一次,API可能没有那么快更新,可等待,无传入参数
+    print(flora_api)
+    print("FloraBot插件模板 加载成功")
+
+
+def event(data: dict):  # 事件函数,FloraBot每收到一个事件都会调用这个函数(若插件已设为禁用则不调用),传入原消息JSON参数
+    print(data)
