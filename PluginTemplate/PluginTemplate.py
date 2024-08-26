4a2ea15fdfee36aa7eda553dacef945486c61236
AEBC08 <125038182+AEBC08@users.noreply.github.com>
2024-08-27 00:41:53 +0800

    Add files via upload

--- PluginTemplate/PluginTemplate.py
+++ PluginTemplate/PluginTemplate.py
@@ -1,3 +1,5 @@
+# 前言,这里用不到的函数可以不定义,可以直接删去,包括API也可以删去不定义,不会报错的
+
 flora_api = {}  # 顾名思义,FloraBot的API,载入(若插件已设为禁用则不载入)后会赋值上
 
 
@@ -21,10 +23,16 @@ def api_update_event():  # 在API更新时会调用一次(若插件已设为禁�
 
 def event(data: dict):  # 事件函数,FloraBot每收到一个事件都会调用这个函数(若插件已设为禁用则不调用),传入原消息JSON参数
     print(data)
+    send_type = data.get("SendType")
+    send_address = data.get("SendAddress")
+    ws_client = send_address.get("WebSocketClient")
+    ws_server = send_address.get("WebSocketServer")
+    send_host = send_address.get("SendHost")
+    send_port = send_address.get("SendPort")
     uid = data.get("user_id")  # 事件对象QQ号
     gid = data.get("group_id")  # 事件对象群号
     mid = data.get("message_id")  # 消息ID
     msg = data.get("raw_message")  # 消息内容
     if msg is not None:
         msg = msg.replace("&#91;", "[").replace("&#93;", "]").replace("&amp;", "&").replace("&#44;", ",")  # 消息需要将URL编码替换到正确内容
-        print(uid, gid, mid, msg)
+        print(send_type, uid, gid, mid, msg, ws_client, ws_server, send_host, send_port)
