19e31c560bf98cc006ee5cf192a2b00009b5bcc9
luyanci <2058282593@qq.com>
2024-09-08 15:25:09 +0800

    feat:exit

--- FloraBot.py
+++ FloraBot.py
@@ -384,12 +384,15 @@ if __name__ == "__main__":
     print(f"欢迎使用 FloraBot {flora_version}")
     print("\033[93m声明: 插件为第三方内容, 请您自行分辨是否为恶意插件, 若被恶意插件入侵/破坏了您的设备或恶意盗取了您的信息, 造成的损失请自负, FloraBotTeam 概不负责也无义务负责!!!\033[0m")
     load_plugins()
-    print(f"框架连接方式为: {connection_type}")
-    if connection_type == "HTTP":
-        flora_server.run(host=flora_host, port=flora_port)
-    elif connection_type == "WebSocket":
-        flora_server_ws = FloraWebsocketServer(host=flora_host, port=flora_port)
-        flora_server_ws.set_fn_new_client(client_connect)
-        flora_server_ws.set_fn_client_left(client_left)
-        flora_server_ws.set_fn_message_received(ws_message_received)
-        flora_server_ws.run_forever()
+    logger.info(f"框架连接方式为: {connection_type}")
+    try:
+        if connection_type == "HTTP":
+            flora_server.run(host=flora_host, port=flora_port)
+        elif connection_type == "WebSocket":
+            flora_server_ws = FloraWebsocketServer(host=flora_host, port=flora_port)
+            flora_server_ws.set_fn_new_client(client_connect)
+            flora_server_ws.set_fn_client_left(client_left)
+            flora_server_ws.set_fn_message_received(ws_message_received)
+            flora_server_ws.run_forever()
+    finally:
+        os._exit(0)
